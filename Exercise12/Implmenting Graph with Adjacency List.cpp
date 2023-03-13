13425a8b4a96992c84a54469cdb0b2d7e7c52c4c
Ceiling_roof <ted10014@hotmail.com>
2023-03-13 13:12:55 +0800

    Committed 2023/03/13

--- Exercise12/Implmenting Graph with Adjacency List.cpp
+++ Exercise12/Implmenting Graph with Adjacency List.cpp
@@ -3,17 +3,28 @@
 #include <algorithm>
 using namespace std;
 
-ostream& operator<<(std::ostream& os, const city c);
+ostream &operator<<(ostream &os, const city c);
 
 enum class city : int {
 	MOSCOW, LONDON, ISTANBUL, SEATTLE, DUBAI, MUMBAI, SINGAPORE
+};
+
+class graph {
+
+public:
+
+	vector<vector<pair<int, int> > > data;
+
+	graph(int n) {
+		data = vector<vector<pair<int, int>>>(n, vector<pair<int, int>>());
+	}
 };
 
 int main() {
 
 }
 
-ostream &operator<<(std::ostream& os, const city c)
+ostream &operator<<(ostream &os, const city c)
 {
 	switch (c)
 	{
