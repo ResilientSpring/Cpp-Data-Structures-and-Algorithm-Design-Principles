b6534be6d094f2eb3a981d3edf9b7ebe3bc3fecd
Ceiling_roof <ted10014@hotmail.com>
2023-03-13 13:05:20 +0800

    Committed 2023/03/13

--- Exercise12/Implmenting Graph with Adjacency List.cpp
+++ Exercise12/Implmenting Graph with Adjacency List.cpp
@@ -3,10 +3,42 @@
 #include <algorithm>
 using namespace std;
 
+ostream& operator<<(std::ostream& os, const city c);
+
 enum class city : int {
-	MOWSCOW, LONDON, ISTANBUL, SEATTLE, DUBAI, MUMBAI, SINGAPORE
+	MOSCOW, LONDON, ISTANBUL, SEATTLE, DUBAI, MUMBAI, SINGAPORE
 };
 
 int main() {
 
+}
+
+ostream &operator<<(std::ostream& os, const city c)
+{
+	switch (c)
+	{
+	case city::MOSCOW:
+		os << "MOSCOW";
+		return os;
+	case city::LONDON:
+		os << "LONDON";
+		return os;
+	case city::ISTANBUL:
+		os << "ISTANBUL";
+		return os;
+	case city::SEATTLE:
+		os << "SEATTLE";
+		return os;
+	case city::DUBAI:
+		os << "DUBAI";
+		return os;
+	case city::MUMBAI:
+		os << "MUMBAI";
+		return os;
+	case city::SINGAPORE:
+		os << "SINGAPORE";
+		return os;
+	default:
+		return os;
+	}
 }
