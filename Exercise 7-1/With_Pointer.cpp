15f8df1c92ec70bd90f0d3a85cff3cfc50386325
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-09 14:21:03 +0800

    Committed 2023/08/09

--- Exercise 7-1/With_Pointer.cpp
+++ Exercise 7-1/With_Pointer.cpp
@@ -18,14 +18,14 @@ int main() {
 /* Notes
 
 1. A class (or struct) can contain a pointer to a class (or struct) of the same type, 
-   but cannot contain an instance of the same type, since that would result in infinite recursion.
+   but cannot contain an instance of the same type, since that would result in infinite recursion.[1]
 
 */
 
 /*
 * 
 * References:
-* 1. 
+* 1. https://stackoverflow.com/questions/54965886/c-incomplete-type-not-allowed-while-declaring-struct-and-class
 
 
 */
