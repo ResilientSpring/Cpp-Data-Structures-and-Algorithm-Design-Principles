7d5d578966b94db5b269bf3a404b5019ed83c885
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-09 14:19:53 +0800

    Committed 2023/08/09

--- Exercise 7-1/With_Pointer.cpp
+++ Exercise 7-1/With_Pointer.cpp
@@ -7,10 +7,25 @@ struct node {
 	
 	string position;
 
-	node *first, *second;
+	node *first, *second; // [Note 1]
 };
 
 
 int main() {
 
 }
+
+/* Notes
+
+1. A class (or struct) can contain a pointer to a class (or struct) of the same type, 
+   but cannot contain an instance of the same type, since that would result in infinite recursion.
+
+*/
+
+/*
+* 
+* References:
+* 1. 
+
+
+*/
