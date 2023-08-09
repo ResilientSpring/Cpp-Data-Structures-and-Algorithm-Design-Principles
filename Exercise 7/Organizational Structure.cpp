1a01cedd7db1aeed7b5e163309c6fedfb334fcb6
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-09 15:49:50 +0800

    Committed on or around 2023/08/09

--- Exercise 7/Organizational Structure.cpp
+++ Exercise 7/Organizational Structure.cpp
@@ -16,6 +16,16 @@ struct orgnization_tree {
 
 	node *root;
 
+	// Add a function to create the root.
+	static orgnization_tree create_organization_structure(const string &pos) {
+
+		orgnization_tree tree;
+		tree.root = new node(pos, NULL, NULL);
+		return tree;
+
+	}
+
+
 };
 
 
