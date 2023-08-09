e526fe89e57a6eb890e773703c5c254acd59fbc5
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-09 16:21:35 +0800

    Committed on or around 2023/08/09

---
+++ Exercise 7 (Reference Parameter)/pos.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <queue>
+#include <string>
+using namespace std;
+
+struct node {
+
+	string position;
+
+	node *first, *second;
+
+};
+
+
+struct orgnization_tree {
+
+	node *root;
+
+	// Add a function to create the root.  This is a static function just to create the tree.
+	static orgnization_tree create_organization_structure(const string &pos) {
+
+		orgnization_tree tree;
+		tree.root = new node{ pos, NULL, NULL };
+		return tree;
+
+	}
+
+
+};
+
+
+int main() {
+
+}
