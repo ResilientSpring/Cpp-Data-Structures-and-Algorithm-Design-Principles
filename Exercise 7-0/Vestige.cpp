3ba5108558c1e61960ee2b4be5474711ec252385
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-08-10 23:41:17 +0800

    Committed on or around 2023/08/10

---
+++ Exercise 7-0/Vestige.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <queue>
+#include <string>
+using namespace std;
+
+struct node {
+
+	string position;
+
+	node *first, *second;
+
+};
+
+
+struct orgnization_tree {
+
+	node *root;
+
+	// Add a function to create the root.  This is a static function just to create the tree.
+	static orgnization_tree create_organization_structure(string pos) {
+
+		orgnization_tree tree;
+		tree.root = new node{ pos, NULL, NULL };
+		return tree;
+
+	}
+
+	// Return a pointer of node type.
+	static node *find(node *root, string value) {
+
+		if (root == NULL)
+			return NULL;
+		if (root->position == value)
+			return root;
+
+		auto firstFound = orgnization_tree::find(root->first, value);
+	}
+
+};
+
+
+int main() {
+
+}
