a78e41ae14e8e074993fcfccb86134811a07200e
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-07-05 21:19:46 +0800

    Committed on or around 2024/07/05

---
+++ Exercise 7 in 2024/applicant.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <queue>
+#include <string>
+using namespace std;
+
+struct node {
+
+	string position;
+
+	node* first;
+	node* second;
+
+};
+
+
+struct orgnization_tree {
+
+	node* root;
+
+	// Add a function to create the root.  This is a static function just to create the tree.
+	static orgnization_tree create_organization_structure(const string &pos) {
+
+		orgnization_tree tree;
+		tree.root = new node{ pos, NULL, NULL };
+		return tree;
+
+	}
+
+	// Write another function that will help us find a particular node based on a value to make our insertion 
+	// function easier.
+	static node *find(node *root, const string &value) {
+
+		if (root == NULL)
+			return NULL;
+		if (root->position == value)
+			return root;
+
+		auto firstFound = orgnization_tree::find(root->first, value);
+
+		if (firstFound != NULL)
+			return firstFound;
+
+		return orgnization_tree::find(root->second, value);
+	}
+
+
+};
+
+
+int main() {
+
+}
